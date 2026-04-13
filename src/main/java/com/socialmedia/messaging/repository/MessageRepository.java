061ce57f6c944f2b4d552e9206bfa879678fce14
Mansi Chate <mancc81@gmail.com>
2026-04-13 10:14:54 +0530

    Messages Repository updated

--- src/main/java/com/socialmedia/messaging/repository/MessageRepository.java
+++ src/main/java/com/socialmedia/messaging/repository/MessageRepository.java
@@ -1,21 +1,17 @@
-package com.socialmedia.messaging.repository;
+package com.socialmedia.repository;
 
-import com.socialmedia.messaging.entity.Message;
+import com.socialmedia.entity.Message;
+import com.socialmedia.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
-
-    @Query("SELECT m FROM Message m WHERE " +
-           "(m.senderID = :userID AND m.receiverID = :otherID) OR " +
-           "(m.senderID = :otherID AND m.receiverID = :userID) " +
-           "ORDER BY m.timestamp ASC")
-    List<Message> findConversation(@Param("userID") int userID,
-                                   @Param("otherID") int otherID);
-
-    List<Message> findByReceiverIDOrderByTimestampDesc(int receiverID);
+    
+    // Find conversation between two users
+    List<Message> findBySenderAndReceiverOrReceiverAndSenderOrderByTimestampAsc(
+        User sender1, User receiver1, User sender2, User receiver2
+    );
 }
