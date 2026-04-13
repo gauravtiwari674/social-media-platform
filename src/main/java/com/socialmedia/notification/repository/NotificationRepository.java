86c82131805ea67146c33ba8517cd827b348512d
Mansi Chate <mancc81@gmail.com>
2026-04-13 10:48:28 +0530

    notification repository added

--- src/main/java/com/socialmedia/notification/repository/NotificationRepository.java
+++ src/main/java/com/socialmedia/notification/repository/NotificationRepository.java
@@ -1,11 +1,18 @@
-package com.socialmedia.notification.repository;
 
-import com.socialmedia.notification.entity.Notification;
+package com.socialmedia.repository;
+
+import com.socialmedia.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-    List<Notification> findByUserIDOrderByTimestampDesc(int userID);
+    List<Notification> findByUser_UserIDOrderByTimestampDesc(int userID);
 }
+
+
+
+
+
