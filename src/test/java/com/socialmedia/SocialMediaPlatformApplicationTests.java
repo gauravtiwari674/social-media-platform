da8397d2564274d9e79b859da4b9391beeedb742
Gaurav Tiwari <gaurav06072004@gmail.com>
2026-04-14 19:43:07 +0530

    Created Test file

---
+++ src/test/java/com/socialmedia/SocialMediaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia;
+
+import com.socialmedia.SocialMediaPlatformApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SocialMediaPlatformApplication.class)
+class SocialMediaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
